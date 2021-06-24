b29c868048768de2cbd1e6ae4f1c14cc2de8882f
urawa72 <urawa72@gmail.com>
2021-06-24 22:17:38 +0900

    implement next-auth signin signout

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,42 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import NextAuth from 'next-auth';
+import Providers from 'next-auth/providers';
+import Adapters from 'next-auth/adapters';
+import { PrismaClient } from '@prisma/client';
+
+let prisma;
+
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
+
+const options = {
+  providers: [
+    Providers.Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  adapter: Adapters.Prisma.Adapter({ prisma }),
+};
+
+const auth = (
+  req: NextApiRequest,
+  res: NextApiResponse
+): void | Promise<void> => NextAuth(req, res, options);
+export default auth;
+
+// export default NextAuth({
+//   providers: [
+//     Providers.Google({
+//       clientId: process.env.GOOGLE_CLIENT_ID,
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//     }),
+//   ],
+//   adapter: Adapters.Prisma.Adapter({ prisma }),
+// });
